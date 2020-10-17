0b627feeb4f624c3ebb0c7365133da6ccdf28914
Mads Nedergaard <nedergaardmads@gmail.com>
2020-10-17 10:58:06 +0200

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Mads Nedergaard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>hi 👋</h1>
+    </div>
+  );
+}
